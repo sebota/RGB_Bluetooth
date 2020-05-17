6552d11b47c515502fb915311396c78ca15669d4
seb28 <seb28@poczta.fm>
2020-05-17 15:51:09 +0200

    Łącznenie z ESP

--- app/src/main/java/com/example/bluetooth/MainActivity.java
+++ app/src/main/java/com/example/bluetooth/MainActivity.java
@@ -2,34 +2,36 @@ package com.example.bluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.ParcelUuid;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button display_devices_button;
+    Button connect_to_esp;
     BluetoothAdapter adapter;
-    TextView devices_text;
+    public OutputStream outputStream;
+    public InputStream inStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        display_devices_button = findViewById(R.id.display_devices_button);
+        connect_to_esp = findViewById(R.id.display_devices_button);
         adapter = BluetoothAdapter.getDefaultAdapter();
-        devices_text = findViewById(R.id.devices_text);
 
         //czy dostepny jest interfejs Bluetooth
         if (adapter != null) {
@@ -47,45 +49,60 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
 
-        display_devices_button.setOnClickListener(new View.OnClickListener() {
+        connect_to_esp.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
                 if(adapter.isEnabled()){
-                    devices_text.setText("Dostępne urządzenia");
-                    Set<BluetoothDevice> devices = adapter.getBondedDevices();
-                    for(BluetoothDevice device: devices){
-                        devices_text.append("\nUrządzenie " + device.getName());
+                    Set<BluetoothDevice> bondedDevices = adapter.getBondedDevices();
+
+                    if(bondedDevices.size() > 0) {
+                        Object[] devices = bondedDevices.toArray();
+                        BluetoothDevice device = (BluetoothDevice) devices[1]; //index
+                        ParcelUuid[] uuids = device.getUuids();
+                        BluetoothSocket socket = null;
+                        try {
+                            socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            assert socket != null;
+                            socket.connect();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            outputStream = socket.getOutputStream();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            inStream = socket.getInputStream();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Bluetooth jest wyłączony",
                             Toast.LENGTH_LONG).show();
                 }
+                Toast.makeText(getApplicationContext(), "Połączono z ESP",
+                        Toast.LENGTH_LONG).show();
+                try {
+                    run();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
+
+
+    public void run() throws IOException {
+        outputStream.write('2');
+    }
+
 }
 
-class watek extends Thread
-{
-    public void run()
-    {
-    //zakładamy inicjalizację urządzenia Bluetooth
-        BluetoothDevice urzadzenie = null; //!!!
-        BluetoothSocket gniazdoBT = null;
-        try {
-    //pobranie gniazda klienta Bluetooth
-            gniazdoBT =
-                    urzadzenie.createRfcommSocketToServiceRecord(UUID.fromString("usluga"));
-        } catch (IOException e) {}
-        try {
-    //próba połączenia z serwerem Bluetooth
-            gniazdoBT.connect();
-        } catch (IOException e) {}
-    //obsługa połączenia
-        try {
-    //zamknięcie gniazda Bluetooth
-            gniazdoBT.close();
-        } catch (IOException e) {}
-    }
-}
